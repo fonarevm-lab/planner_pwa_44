1a5fbe684eb4f2da7a8ece84670196cd71be081b
Фонарев Михаил Владимирович <fonarevmv@ar.int>
2026-07-31 00:43:04 +0500

    Deploy PWA

---
+++ assets/NoteCreateView-RnKT2MuQ.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./NoteCreateView.vue_vue_type_script_setup_true_lang-DqDDUlrb.js";import"./index-BUKs6C5I.js";export{m as default};
--- src/views/NoteDetailView.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-// Алиас для NoteCreateView в режиме редактирования.
-// Маршрут /notes/:id просто рендерит NoteCreateView с подгруженной заметкой.
-import NoteCreateView from './NoteCreateView.vue'
-</script>
-
-<template>
-  <NoteCreateView />
-</template>
