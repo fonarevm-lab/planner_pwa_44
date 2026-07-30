dc589bfc2a89fb258bd759bc511cf68834d317b8
Фонарев Михаил Владимирович <fonarevmv@ar.int>
2026-07-30 23:44:21 +0500

    Add built dist

---
+++ dist/assets/NoteCreateView-BCQEKGaJ.js
@@ -0,0 +1,1 @@
+import{_ as m}from"./NoteCreateView.vue_vue_type_script_setup_true_lang-cIhOGixj.js";import"./index-C2hFf_Bb.js";export{m as default};
---
+++ dist/assets/categories-DqKl5TSG.js
@@ -0,0 +1,1 @@
+import{B as s,j as r,D as o}from"./index-C2hFf_Bb.js";const t=r(o);let a=!1;function l(){return{items:t,async load(){if(a)return;const e=await s();e.length&&(t.value=e),a=!0}}}export{l as u};
